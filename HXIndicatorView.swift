db75d27a55a7cb642c710cd82e4b354873df0c50
SYkehaoran <18510666543@163.com>
2019-08-02 20:25:18 +0800

    增加lineCap

--- HXIndicatorView.swift
+++ HXIndicatorView.swift
@@ -6,17 +6,18 @@
 //  Copyright © 2019 China Asset Management Co., Ltd. All rights reserved.
 //
 /*
-  继承ShaperLayerView，根据宽高自定义右侧的箭头指示器。
+ 继承ShaperLayerView，根据宽高自定义右侧的箭头指示器。
  */
 import UIKit
 
 class HXIndicatorView: ShaperLayerView {
-
+    
     override func draw(_ rect: CGRect) {
         
         let size = rect.size;
         shaperLayer().lineWidth = 1;
         shaperLayer().fillColor = UIColor.clear.cgColor;
+        shaperLayer().lineCap = kCALineCapRound
         
         let path = UIBezierPath.init()
         path.move(to: CGPoint(x: 0, y: 0))
