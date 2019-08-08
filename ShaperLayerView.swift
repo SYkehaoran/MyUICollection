4d9d91e3abd60d0121370dc1946036d13a9aa14c
SYkehaoran <18510666543@163.com>
2019-08-08 09:36:20 +0800

    去掉shaperLayer里的本地引用

--- ShaperLayerView.swift
+++ ShaperLayerView.swift
@@ -4,10 +4,9 @@
 //  Created by 柯浩然 on 2019/7/31.
 //
 /*
- 把shaperLayer做为寄宿图层的View。
- 1.可以支持不同角度的圆角。系统可以在iOS11之后使用layer.maskedCorners支持
- 2.用shaperLayer画图性能更高
- 3.可以解决部分圆形视图不够圆润的问题
+ 把shaperLayer做为寄宿图层的View
+ 1.用shaperLayer画图性能更高
+ 2.可以解决部分圆形视图不够圆润的问题
  */
 import UIKit
 
@@ -30,12 +29,7 @@ class ShaperLayerView: UIView {
     
     override func draw(_ rect: CGRect) {
         
-        if let array = cornerRadiusArray {
-            
-            let roundPath = UIBezierPath(roundedRect: rect, cornerRadiusArray: array, lineWidth: 0)
-            
-            shaperLayer().path = roundPath?.cgPath
-        }else if shaperLayer().cornerRadius != 0 {
+        if shaperLayer().cornerRadius != 0 {
             let roundPath = UIBezierPath(roundedRect: rect, cornerRadius: shaperLayer().cornerRadius)
             shaperLayer().path = roundPath.cgPath
         }
